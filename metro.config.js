db577abc5ee02b49f92ada93ef361e0b5a9d0ac9
nourabagdadi1 <160722650+nourabagdadi1@users.noreply.github.com>
2024-03-16 07:14:18 +0300

    Add files via upload

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,9 @@
- // metro.config.js
-module.exports = {
-    // Configuration options
+const { getDefaultConfig } = require('@react-native/metro-config');
+
+module.exports = (async () => {
+  const defaultConfig = await getDefaultConfig(__dirname);
+  return {
+    ...defaultConfig,
+    // Add any additional configuration options here
   };
-  
+})();
