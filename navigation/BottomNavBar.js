6af1d344327341d5a7b98935271c7c0fc607844a
nourabagdadi1 <160722650+nourabagdadi1@users.noreply.github.com>
2024-03-21 15:09:05 +0300

    Add files via upload

--- navigation/BottomNavBar.js
+++ navigation/BottomNavBar.js
@@ -6,26 +6,36 @@ import { signOut } from 'firebase/auth'
 import { auth } from '../config/firebase'
 import { useNavigation } from '@react-navigation/native';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { useIsFocused } from '@react-navigation/native';
 
 export default function BottomNavBar() {
 
     const navigation = useNavigation();
+    const isFocused = useIsFocused();
 
-    const handlePressHome = () => {
-      navigation.navigate('Home');
-    };
-  
-    const handlePressAnalytics = () => {
-      navigation.navigate('Analytics');
-    };
-  
-    const handlePressSettings = () => {
-      navigation.navigate('Settings');
-    };
-  
-    const handlePressRecommendations = () => {
-      navigation.navigate('Recommendations');
-    };
+
+  //without checking the screens inside the settings screen
+    /*const getCurrentRouteName = () => {
+      const currentRoute = navigation?.getState()?.routes[navigation?.getState()?.index]?.name;
+      return currentRoute;
+  };*/
+
+  //with checking the screens inside the settings screen
+  const getCurrentRouteName = () => {
+    const routes = navigation?.getState()?.routes;
+    const currentIndex = navigation?.getState()?.index;
+    if (routes && currentIndex !== undefined && currentIndex < routes.length) {
+        // Check if any route in the stack belongs to the "Settings" stack
+        for (let i = currentIndex; i >= 0; i--) {
+            if (routes[i].name === 'Settings') {
+                return 'Settings';
+            }
+        }
+        // If not, return the name of the current route
+        return routes[currentIndex].name;
+    }
+    return null;
+};
     
     const handleLogout = async ()=>{
       await signOut(auth);
@@ -35,18 +45,30 @@ export default function BottomNavBar() {
         <SafeAreaView style={styles.SafeAreaView}>
         <View style={styles.container}>
         <View style={styles.navBarBottom}>
-        <TouchableOpacity onPress={handlePressSettings}>
-            <Image source={require('../assets/icons/newsettings.png')} style={styles.logo} />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={handlePressRecommendations}>
-            <Image source={require('../assets/icons/newrecommendations.png')} style={styles.logo} />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={handlePressAnalytics}>
-            <Image source={require('../assets/icons/newanalytics.png')} style={styles.logo} />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={handlePressHome}>
-            <Image source={require('../assets/icons/newhome.png')} style={styles.logo} />
-        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
+                        <Image
+                            source={(getCurrentRouteName() === 'Settings' && isFocused) ? require('../assets/icons/newsettings_active.png') : require('../assets/icons/newsettings.png')}
+                            style={styles.logo}
+                        />
+                    </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigation.navigate('Recommendations')}>
+                <Image
+                    source={getCurrentRouteName() === 'Recommendations' ? require('../assets/icons/newrecommendations_active.png') : require('../assets/icons/newrecommendations.png')}
+                    style={styles.logo}
+                />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigation.navigate('Analytics')}>
+                <Image
+                    source={getCurrentRouteName() === 'Analytics' ? require('../assets/icons/newanalytics_active.png') : require('../assets/icons/newanalytics.png')}
+                    style={styles.logo}
+                />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+                <Image
+                    source={getCurrentRouteName() === 'Home' ? require('../assets/icons/newhome_active.png') : require('../assets/icons/newhome.png')}
+                    style={styles.logo}
+                />
+            </TouchableOpacity>
         </View>
         </View>
       </SafeAreaView>
