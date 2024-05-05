390b9a77603aae8d23ac1d90ed66ac7441b67a3f
alanoudii11 <alanoudd411@gmail.com>
2024-05-05 17:21:52 +0300

    update

---
+++ charts/W_WeeklyChart.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { ScrollView, View } from 'react-native';
+import { BarChart } from 'react-native-chart-kit';
+
+const W_WeeklyChart = () => {
+  const chartData = {
+    labels: ['الأحد', 'الاثنين', 'الثلاثاء', 'الأربعاء', 'الخميس', 'الجمعة', 'السبت'],
+    datasets: [{
+      data: [10, 20, 30, 40, 50, 30, 45].map(kWh => kWh * 1000) // Convert kWh to watts
+    }]
+  };
+
+  const chartWidth = 350; 
+
+  return (
+    <ScrollView horizontal showsHorizontalScrollIndicator={true}>
+      <View>
+        <BarChart
+          data={chartData}
+          width={chartWidth} 
+          height={280}
+          yAxisLabel=""
+          yAxisSuffix=" واط" // Changed to "W" for watts
+          chartConfig={{
+            backgroundColor: '#f2f2f2',
+            backgroundGradientFrom: '#f2f2f2',
+            backgroundGradientTo: '#f2f2f2',
+            decimalPlaces: 0, // Might want to adjust for whole numbers since dealing with watts
+            color: (opacity = 1) => `rgba(0, 163, 255, ${opacity})`,
+            labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+            style: {
+              borderRadius: 16
+            },
+            barPercentage: 0.5,
+          }}
+          style={{
+            marginVertical: 8,
+            borderRadius: 16
+          }}
+          fromZero
+        />
+      </View>
+    </ScrollView>
+  );
+};
+
+export default W_WeeklyChart;
