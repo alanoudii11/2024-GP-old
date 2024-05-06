efcaedcdf81044ef62188c736d8f09aa11dd79f3
nourabagdadi1 <160722650+nourabagdadi1@users.noreply.github.com>
2024-05-06 22:59:02 +0300

    Update RecommendationsScreen.js

--- screens/RecommendationsScreen.js
+++ screens/RecommendationsScreen.js
@@ -1,97 +1,115 @@
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, ScrollView, FlatList } from 'react-native';
 import { StyleSheet, Image } from "react-native";
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { themeColors } from '../theme';
 import TopNavBar from '../navigation/TopNavBar';
 import BottomNavBar from '../navigation/BottomNavBar';
+import { useNavigation } from '@react-navigation/native';
+import * as Icons from 'react-native-heroicons/outline';
+
+
+import { auth} from '../config/firebase'
+import { collection, query, where, getDocs, doc } from 'firebase/firestore';
+import { db } from '../config/firebase';
 
 export default function RecommendationsScreen() {
-  return (
-    <View style={{ flex: 1 }}>
-      <TopNavBar />
-      <ScrollView contentContainerStyle={styles.scrollContent}>
-      <View style={styles.sugg}>
-        <Text style={styles.descriptionText}>اقتراحات</Text>
-      </View>
-      
-      
-        <View style={styles.recommendationsContent}>
-          <View style={styles.recommendationItem}>
-            <Text style={styles.recommendationText}>إغلاق أفياش الكهرباء يقلل ٢٠٪ من استهلاك الطاقة</Text>
-            <Image source={require('../assets/icons/recommendations.png')} style={[styles.recommendationIcon, { tintColor: 'yellow' }]} />
-          </View>
-          <View style={styles.recommendationItem}>
-            <Text style={styles.recommendationText}>إغلاق الأجهزة الإلكترونية عند عدم الاستخدام يمكن أن يوفر كميات كبيرة من الطاقة.</Text>
-            <Image source={require('../assets/icons/recommendations.png')} style={[styles.recommendationIcon, { tintColor: 'yellow' }]} />
-          </View>
-          <View style={styles.recommendationItem}>  
-            <Text style={styles.recommendationText}>إغلاق أفياش الكهرباء يقلل ٢٠٪ من استهلاك الطاقة</Text>
-            <Image source={require('../assets/icons/recommendations.png')} style={[styles.recommendationIcon, { tintColor: 'yellow' }]} />
-          </View>
-          <View style={styles.recommendationItem}>  
-            <Text style={styles.recommendationText}>إغلاق أفياش الكهرباء يقلل ٢٠٪ من استهلاك الطاقة</Text>
-            <Image source={require('../assets/icons/recommendations.png')} style={[styles.recommendationIcon, { tintColor: 'yellow' }]} />
-          </View>
-          <View style={styles.recommendationItem}>
-            <Text style={styles.recommendationText}>إغلاق أفياش الكهرباء يقلل ٢٠٪ من استهلاك الطاقة</Text>
-            <Image source={require('../assets/icons/recommendations.png')} style={[styles.recommendationIcon, { tintColor: 'yellow' }]} />
-          </View>
-          <View style={styles.recommendationItem}>
-            <Text style={styles.recommendationText}>إغلاق الأجهزة الإلكترونية عند عدم الاستخدام يمكن أن يوفر كميات كبيرة من الطاقة.</Text>
-            <Image source={require('../assets/icons/recommendations.png')} style={[styles.recommendationIcon, { tintColor: 'yellow' }]} />
-          </View>
-        </View>
-      </ScrollView>
 
-      <BottomNavBar />
-    </View>
+
+
+  const navigation = useNavigation();
+  const [recommendations, setTips] = useState([]);
+
+  useEffect(() => {
+    const fetchTips = async () => {
+      try {
+        const tipsRef = collection(db, 'recommendations');
+        const querySnapshot = await getDocs(tipsRef);
+        const tipData = querySnapshot.docs.map(doc => ({
+          id: doc.id,
+          tip: doc.data().tip
+        }));
+        setTips(tipData);
+      } catch (error) {
+        console.error('Error fetching tips:', error);
+      }
+    };
+
+
+
+    fetchTips(); // Fetch tips  regardless of condition
+
+      }, []);
+
+
+
+  return (
+    <View style={styles.container}>
+
+      <TopNavBar />
+
+      <ScrollView style={{ flex: 1 }}>
+
+      <View style={styles.tipsSection}>
+      <Text style={styles.header}>اقتراحات</Text>
+ {recommendations.map(item => (
+            <View key={item.id} style={styles.tipItem}>
+              <View style={styles.contentContainer}>
+                <Text style={styles.tipTitle}>{item.tip}</Text>
+                <Image source={require('../assets/icons/recommendations.png')} style={[styles.recommendationIcon, { tintColor: 'yellow' }]} />
+              </View>
+            </View>
+          ))}
+      </View>
+      </ScrollView>
+      <BottomNavBar/>
+      </View>
   );
 }
 
+
 const styles = StyleSheet.create({
-  scrollContent: {
-    flexGrow: 1,
-    paddingBottom: 100, 
+  container: {
+    flex: 1,
+    //backgroundColor: themeColors.bg, // Set background color
   },
-  recommendationsContent: {
-    marginTop: 20,
-    marginHorizontal: 20,
+  tipsSection:
+  {
+    margin: 20,
   },
-  sugg: {
-    alignItems: 'flex-end', 
-    paddingTop: 20,
-    marginHorizontal: 20,
+  header: {
+    fontSize: 20, // Adjust font size
+    fontWeight: 'bold',
+    color: '#143638',
+    textAlign: 'right', // Align text to the right
+    marginBottom: 20,
   },
-  recommendationItem: {
+  tipItem: {
     backgroundColor: themeColors.bg, 
-    padding: 10,
-    borderRadius: 15,
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    paddingHorizontal: 20, 
-    marginBottom: 35,
-    alignItems: 'center',
+    padding: 20,
+    marginBottom: 10,
+    borderRadius: 15, // Adjust border radius
+    //textAlign: 'right', // Align text to the right
     
+  },
+  tipTitle: {
+    fontSize: 16, // Adjust font size
+    textAlign: 'right', // Align text to the right
+    color: '#FFFFFF',
+   // marginRight: 10,
+   //backgroundColor: 'blue', // Set background color
+   flex: 1, // Allow the title to expand and fill available space
+  },
+  contentContainer: {
+    //alignItems: 'center',
+    //backgroundColor: 'pink', // Set background color
+    flexDirection: 'row',
+
   },
   recommendationIcon: {
     width: 30,
     height: 30,
-    marginLeft: 5,
-    
-  },
-  recommendationText: {
-    fontSize: 16,
-    //fontWeight: 'bold',
-    color: '#FFFFFF',
-    textAlign: 'right',
-    padding: 7,
-    marginLeft: 20,
-  },
-  descriptionText: {
-    fontSize: 20,
-    color: '#143638', 
-    textAlign: 'right',
-    fontWeight: 'bold',
+    marginLeft: 10,
 
+    
   },
 });
